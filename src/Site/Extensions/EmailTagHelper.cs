bb6dbcbc63fc7c02ebf39e5a35db504199a115fd
Aron Rissato <aron_rissato@hotmail.com>
2021-04-16 17:21:43 -0300

    TagHelper passando parâmetros.

--- src/Site/Extensions/EmailTagHelper.cs
+++ src/Site/Extensions/EmailTagHelper.cs
@@ -8,11 +8,13 @@ namespace Site.Extensions
 {
     public class EmailTagHelper : TagHelper
     {
+        public string EmailDomain { get; set; } = "hotmail.com";
+
         public override async Task ProcessAsync(TagHelperContext context, TagHelperOutput output)
         {
             output.TagName = "a";
             var content = await output.GetChildContentAsync();
-            var target = content.GetContent() + "@" + "hotmail.com";
+            var target = content.GetContent() + "@" + EmailDomain;
             output.Attributes.SetAttribute("href", "mailto:" + target);
             output.Content.SetContent(target);
         }
